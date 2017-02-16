8ee346c4450ae0c53a3dff88ab775af408e763f9
Blake Netzeband <blake.netzeband@gmail.com>
2017-02-15 21:55:45 -0700

    added responsiveness to landing page

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react'
 class LandingPage extends Component {
   render(){
     return (
-      <div className="landing-page">
-          <h2 className="landing-text title is-2">
+      <div className="landing-page columns">
+        <div className="landing-page-inner column is-10 is-offset-1">
+          <h2 className="landing-text title is-2 column is-11">
             <span className="accent">Water</span> is the secret to good <span className="accent">health</span>, glowing <span className="accent">skin</span>, and delicious <span className="accent">food</span>.
-            Learn how purified water can better your life.
+            Learn how purified water can improve your life.
           </h2>
-          <h3 className="landing-request subtitle is-2"><a href="#">Request your free quote now</a></h3>
+          <h3 className="landing-request subtitle is-2 column is-11"><a href="#">Request your free quote now</a></h3>
+        </div>
       </div>
     )
   }
